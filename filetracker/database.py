d1af60d7e47f0348633600439fde4e377d2f8363
Kyuzial <kyuzial@protonmail.com>
2023-08-01 20:24:44 +0200

    Basic DB implementation

---
+++ filetracker/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+from bot.utils.config_reader import ConfigReader
+
+config = ConfigReader()
+dbConfig = config.get("filetracker")
+
+
+class Database(object):
+    DATABASE = dbConfig["dbPath"]
+
+    def __init__(self):
+        self.connection = sqlite3.connect(self.DATABASE)
+        self.cursor = self.connection.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    with Database() as db:
+        db.cursor.execute("SELECT * FROM files")
+        print(db.cursor.fetchall())
