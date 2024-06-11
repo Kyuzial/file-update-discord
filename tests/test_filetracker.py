a80e6e584b96f09148ac23d6fbdbcf97b78d0eea
Kyuzial <kyuzial@protonmail.com>
2024-06-11 11:59:55 +0200

    Fix: Update URLs used in tests

--- tests/test_filetracker.py
+++ tests/test_filetracker.py
@@ -7,8 +7,8 @@ from file_update_discord.filetracker.filetracker import (
 
 class TestFile:
     def test_init_valid_url(self):
-        file = File("https://getsamplefiles.com/download/txt/sample-1.txt", 1)
-        assert file.url == "https://getsamplefiles.com/download/txt/sample-1.txt"
+        file = File("https://raw.githubusercontent.com/Kyuzial/file-update-discord/main/LICENSE", 1)
+        assert file.url == "https://raw.githubusercontent.com/Kyuzial/file-update-discord/main/LICENSE"
         assert file.userId == 1
 
     def test_init_invalid_url(self):
@@ -16,11 +16,11 @@ class TestFile:
             File("not a url", 1)
 
     def test_getFileName(self):
-        file = File("https://getsamplefiles.com/download/txt/sample-1.txt", 1)
-        assert file.fileName == "sample-1.txt"
+        file = File("https://raw.githubusercontent.com/Kyuzial/file-update-discord/main/LICENSE", 1)
+        assert file.fileName == "LICENSE"
 
     def test_getHash(self):
-        file = File("https://getsamplefiles.com/download/txt/sample-1.txt", 1)
+        file = File("https://raw.githubusercontent.com/Kyuzial/file-update-discord/main/LICENSE", 1)
         assert isinstance(file.hash, str)
 
     def test_getHash_download_error(self):
